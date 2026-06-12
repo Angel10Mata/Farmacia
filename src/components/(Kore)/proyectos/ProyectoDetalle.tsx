a35df7df484ed8abd9e4145ef8cb4b8198396b5a
Angel10Mata <angelmata112@gmail.com>
2026-06-12 15:56:04 -0600

    style: increase top padding on mobile to pt-32 to reveal title under layout headers

--- src/components/(Kore)/proyectos/ProyectoDetalle.tsx
+++ src/components/(Kore)/proyectos/ProyectoDetalle.tsx
@@ -369,7 +369,7 @@ export default function ProyectoDetalle({ proyecto: proyectoProp }: ProyectoDeta
   ].filter(d => d.value > 0);
 
   return (
-    <div className="w-full max-w-7xl mx-auto space-y-6 px-4 pt-20 pb-16 md:px-8 md:pt-20">
+    <div className="w-full max-w-7xl mx-auto space-y-6 px-4 pt-32 pb-16 md:px-8 md:pt-20">
       {/* Dynamic Browser Tab Title */}
       <title>{`Detalle de Proyecto: ${proyecto.nombre} | KORE BMS`}</title>
 
